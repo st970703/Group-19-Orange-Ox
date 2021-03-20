4fb4278e1bffcaca02788d0feac8d7860f43e651
Justin Kim <naznsan@gmail.com>
2021-03-20 14:58:00 +1300

    Initialize directories

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+	res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+	console.log(`Express server open at ${port}`);
+});
