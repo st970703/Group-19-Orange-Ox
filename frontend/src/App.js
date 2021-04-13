7f34c243c288fb5bdad463776348d4d9372e8242
Callum <callumbradding@gmail.com>
2021-04-13 13:00:06 +1200

    Functional auth0, however need to fix UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Main from "./views/Main";
 import About from "./views/About";
 import Canvas from "./Canvas";
 import Friends from "./views/Friends";
 import Profile from "./views/Profile";
-import ExternalApi from "./views/ExternalApi";
-import AuthenticationButton from './components/authentication-button';
 import { useAuth0 } from '@auth0/auth0-react';
 import Loading from './components/loading';
 import ProtectedRoute from "./auth/protected-route";
+import NavBar from './components/nav-bar';
 
 import './App.css';
 
@@ -20,16 +20,18 @@ const App = () => {
   }
 
   return (
-      <div id="app">
-        <Switch>
-          <Route path='/'>
-            <AuthenticationButton />
+      <div id="app" className="d-flex flex-column h-100">
+      <NavBar />
+        <div className="mt-5">
+          <Switch>
+          <Route path='/' component={Main}>
+            <Route path='/about' component={About} />
+            <ProtectedRoute path='/profile' component={Profile} />
+            <ProtectedRoute path='/canvas' component={Canvas} />
+            <ProtectedRoute path='/friends' component={Friends} />
           </Route>
-          <Route path='/about' component={About} />
-          <ProtectedRoute path='/profile' component={Profile} />
-          <ProtectedRoute path='/canvas' component={Canvas} />
-          <ProtectedRoute path='/friends' component={Friends} />
-        </Switch>
+          </Switch>
+        </div>
       </div>
   );
 }
---
+++ frontend/src/views/Main.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+
+const Main = () => (
+  <div>
+      {this.props.children}
+  </div>
+);
+
+export default Main;
