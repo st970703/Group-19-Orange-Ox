47e57e46c115befd825a59d4936257022f2603bc
st970703 <st970703@users.noreply.github.com>
2021-05-05 14:17:12 +1200

    Moved Toolbar outside Canvas.js and to its own file

--- frontend/src/views/Canvas.js
+++ frontend/src/views/Canvas.js
@@ -1,16 +1,12 @@
 import React, { useContext } from 'react';
-import { FaSave, FaRegTrashAlt } from 'react-icons/fa';
-import styles from './Canvas.module.css';
 import CanvasView from '../components/CanvasView';
-import { brushes } from '../config/CanvasConfig';
 import { CanvasContext } from '../context/CanvasContextProvider';
-import Tooltip from '@material-ui/core/Tooltip';
 import { useMediaQuery } from 'react-responsive';
 import Box from '@material-ui/core/Box';
 import SmallScreenMsg from '../components/SmallScreenMsg';
 import WarningSnackBar from '../components/WarningSnackBar';
-import ColourBar from '../components/ColourBar';
-import { capitalise } from '../config/helperFunctions';
+import Colourbar from '../components/Colourbar';
+import Toolbar from '../components/Toolbar';
 
 
 function Canvas() {
@@ -18,10 +14,7 @@ function Canvas() {
     emitData,
     setSave,
     setClear,
-    isCanvasBlank,
-    brush,
-    setBrush,
-    setWeight
+    isCanvasBlank
   } = useContext(CanvasContext);
 
   const [openSBar, setOpenSBar] = React.useState(false);
@@ -43,12 +36,6 @@ function Canvas() {
     }
   }
 
-  function handleSetBrush(brush_i) {
-    setBrush(brush_i.brushType);
-    setWeight(brush_i.weight);
-  }
-
-
   const canvasWidth = 960;
   const canvasHeight = canvasWidth / (16 / 9);
 
@@ -68,44 +55,12 @@ function Canvas() {
               <CanvasView canvasWidth={canvasWidth}
                 canvasHeight={canvasHeight} />
 
-              <ColourBar />
+              <Colourbar />
 
             </Box>
 
-            <Box display="flex" flexDirection="column" justifyContent="center">
-              <Box display="flex" flexDirection="row">
-                <Tooltip title="Save">
-                  <button className={styles.toolbarButton}
-                    onClick={() => handleSave()}>
-                    <FaSave className={styles.toolbarButtonIcon} />
-                  </button>
-                </Tooltip>
-
-                <Tooltip title="Clear">
-                  <button className={styles.toolbarButton}
-                    onClick={() => handleClear()}>
-                    <FaRegTrashAlt className={styles.toolbarButtonIcon} />
-                  </button>
-                </Tooltip>
-              </Box>
-
-              <div className={styles.brushbar}>
-                <div className={styles.brushes}>
-                  {brushes.map((brush_i, i) => (
-                    <Tooltip title={capitalise(brush_i.brushType)}
-                      key={capitalise(brush_i.brushType) + "_tooltip"}>
-                      <div className={styles.brush} key={i}
-                        onClick={() => handleSetBrush(brush_i)}
-                        style={brush === brush_i.brushType ?
-                          { boxShadow: "0px 0px 0px 4px black inset" } : {}}>
-                        {brush_i.icon}
-                      </div>
-                    </Tooltip>
-                  ))}
-                </div>
-              </div>
-
-            </Box>
+            <Toolbar handleClear={handleClear}
+              handleSave={handleSave} />
 
           </Box>
 
