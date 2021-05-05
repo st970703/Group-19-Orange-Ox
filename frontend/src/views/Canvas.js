c269d62d56978a4396c3cce2d576b25f73826d77
st970703 <st970703@users.noreply.github.com>
2021-05-05 16:47:29 +1200

    Renamed module names from 'ColourBar' to 'Colourbar'

--- frontend/src/views/Canvas.js
+++ frontend/src/views/Canvas.js
@@ -5,7 +5,7 @@ import { useMediaQuery } from 'react-responsive';
 import Box from '@material-ui/core/Box';
 import SmallScreenMsg from '../components/SmallScreenMsg';
 import WarningSnackBar from '../components/WarningSnackBar';
-import Colourbar from '../components/ColourBar';
+import Colourbar from '../components/Colourbar';
 import Toolbar from '../components/Toolbar';
 
 
